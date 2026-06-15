11407179eb111a80d22169c9359aa56cb82afde6
Leonardo Cabrera <u202415820@upc.edu.pe>
2026-06-15 12:38:34 -0500

    fix(database): temporary fix to ensure database is deleted before migration

--- WebWarriors.Aquanetix.Platform/Program.cs
+++ WebWarriors.Aquanetix.Platform/Program.cs
@@ -87,6 +87,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    
+    context.Database.EnsureDeleted();
     context.Database.Migrate();
 }
 
