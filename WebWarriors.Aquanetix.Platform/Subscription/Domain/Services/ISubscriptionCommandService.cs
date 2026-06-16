0132553e300abf376c7367d5eb5ffeaabd6c90df
Sebastian Cochachi <sebastiancochachi02@gmail.com>
2026-06-16 14:02:27 -0500

    feat: implement subscriptions bounded context and fix database connection issues

--- WebWarriors.Aquanetix.Platform/Subscription/Domain/Services/ISubscriptionCommandService.cs
+++ WebWarriors.Aquanetix.Platform/Subscription/Domain/Services/ISubscriptionCommandService.cs
@@ -6,4 +6,6 @@ public interface ISubscriptionCommandService
 {
     Task<WebWarriors.Aquanetix.Platform.Subscription.Domain.Model.Aggregates.Subscription?>
         Handle(CreateSubscriptionCommand command);
+    Task<WebWarriors.Aquanetix.Platform.Subscription.Domain.Model.Aggregates.Subscription?>
+        Handle(CancelSubscriptionCommand command);
 }
