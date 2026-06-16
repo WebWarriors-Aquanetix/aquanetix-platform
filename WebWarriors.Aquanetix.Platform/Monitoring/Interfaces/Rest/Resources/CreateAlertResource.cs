452d5b0a2ba6f54a0393d19c153d0a3d2c018e20
NicoCSE <u20241D428@upc.edu.pe>
2026-06-16 16:41:04 -0500

    feat(monitoring): implement CreateAlert command and endpoint

--- WebWarriors.Aquanetix.Platform/Monitoring/Interfaces/Rest/Resources/CreateAlertResource.cs
+++ WebWarriors.Aquanetix.Platform/Monitoring/Interfaces/Rest/Resources/CreateAlertResource.cs
@@ -1,13 +1,13 @@
-﻿/*namespace WebWarriors.Aquanetix.Platform.Monitoring.Interfaces.Rest.Resources;
+﻿namespace WebWarriors.Aquanetix.Platform.Monitoring.Interfaces.Rest.Resources;
 
 public record CreateAlertResource(
-    int    DeviceId,
-    string DeviceName,
-    string Location,
-    string Type,
-    string Severity,
-    string Message,
+    int            DeviceId,
+    string         DeviceName,
+    string         Location,
+    string         Type,
+    string         Severity,
+    string         Message,
     DateTimeOffset Timestamp,
-    string Status,
-    double Value,
-    double Threshold);*/
+    string         Status,
+    double         Value,
+    double         Threshold);
