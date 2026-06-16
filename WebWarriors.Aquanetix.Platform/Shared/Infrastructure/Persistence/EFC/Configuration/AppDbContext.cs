a5ecc49363c80a58e2dcdc7e6ec1f4fa8243c393
DeterminedSoul7 <u202411402@upc.edu.pe>
2026-06-15 20:35:35 -0500

    fix(get-all-devices): fix app db context for new feature.

--- WebWarriors.Aquanetix.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ WebWarriors.Aquanetix.Platform/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -1,21 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using WebWarriors.Aquanetix.Platform.Dashboard.Infrastructure.Persistence.EFC.Configuration.Extensions;
-using WebWarriors.Aquanetix.Platform.Devices.Domain.Model.Aggregates;
 using WebWarriors.Aquanetix.Platform.Devices.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using WebWarriors.Aquanetix.Platform.Monitoring.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using WebWarriors.Aquanetix.Platform.ServiceDesign.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using WebWarriors.Aquanetix.Platform.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 using WebWarriors.Aquanetix.Platform.Shared.Infrastructure.Persistence.EFC.Interceptors;
-using WebWarriors.Aquanetix.Platform.Monitoring.Domain.Model.Aggregates;
 
 namespace WebWarriors.Aquanetix.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 public class AppDbContext(DbContextOptions options) : DbContext(options)
 {
-   
-    public DbSet<Alert> Alerts { get; set; }
-    public DbSet<Device> Devices { get; set; }
-
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
         builder.AddInterceptors(new AuditableEntityInterceptor());
@@ -26,10 +20,9 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
     {
         base.OnModelCreating(builder);
         builder.UseSnakeCaseNamingConvention();
-
-        builder.ApplyServiceDesignConfiguration();
-        builder.ApplyMonitoringConfiguration();
         builder.ApplyDashboardConfiguration();
         builder.ApplyDevicesConfiguration();
+        builder.ApplyMonitoringConfiguration();
+        builder.ApplyServiceDesignConfiguration();
     }
 }
